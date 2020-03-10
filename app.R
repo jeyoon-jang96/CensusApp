251ddcfa91a16cbd48cfa592ec301c5145ced6f3
Jeyoon Jang <rosariajang96@gmail.com>
2020-03-10 15:55:18 -0500

    !!

--- app.R
+++ app.R
@@ -4,7 +4,7 @@ library(maps)
 library(mapproj)
 
 # Load data ----
-counties <- readRDS("counties.rds")
+counties <- readRDS("data/counties.rds")
 
 # Source helper functions -----
 source("helpers.R")
